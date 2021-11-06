117fc63300842a85b1883e4fb3ba8ebd543eb1af
zi yon <azureuser@zyon.dd0ekl4zqy1ezj5cmosl14vhta.bx.internal.cloudapp.net>
2021-11-06 07:41:42 +0000

    simplified get_headers() and variable naming

--- splitwise.py
+++ splitwise.py
@@ -10,16 +10,15 @@ auth_url = "https://secure.splitwise.com/api/v3.0/"
 
 
 def get_headers():
-    access_token = os.getenv("API_KEY")
-    headers = {"Authorization": "Bearer {token}".format(token=access_token)}
+    headers = {"Authorization": "Bearer {token}".format(token=os.getenv("API_KEY"))}
     return headers
 
 
 def get_current_user():
-    access_token = get_headers()
+    headers = get_headers()
     r = requests.get(
         auth_url + "get_current_user",
-        headers=access_token,
+        headers=headers,
     )
     r_json = r.json()
     return json.dumps(r_json, indent=2)  # formatted with json.dumps for development
