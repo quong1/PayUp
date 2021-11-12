a963d00d7ac9b4b5802c42b8021b8769eb8e33a8
Michael Uong <quong1@student.gsu.com>
2021-11-11 21:29:56 -0500

    Added new files in static for user to store profile pictures

--- forms.py
+++ forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from flaskblog.models import User
+from models import Userdb
 
 
 class RegistrationForm(FlaskForm):
